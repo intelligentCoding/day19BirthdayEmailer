751a7942377ed1618e99c9b0a26b175514337c26
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-01-18 18:14:31 -0500

    Automated birthday wisher project finished

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
 import smtplib
 import datetime as dt
+import random
 
-MY_PASSWORD = "pythonisbest"
 MY_EMAIL = "tazwiztesting2@gmail.com"
+MY_PASSWORD = "pythonisbest"
 
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
 
-try:
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(MY_EMAIL, MY_PASSWORD)
-    server.sendmail(MY_EMAIL, "kashif@mailinator.com", "Hellooo")
-    server.close()
+    print(quote)
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(MY_EMAIL, MY_PASSWORD)
+        server.sendmail(MY_EMAIL, MY_EMAIL, f"Subject:Monday Motivation\n\n{quote}")
+        server.close()
 
-    print('Email sent!')
-except:
-    print('Something went wrong...')
-
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(MY_EMAIL, MY_PASSWORD)
-#     connection.sendmail(
-#         from_addr=MY_EMAIL,
-#         to_addrs="kaaf_kaaf@hotmail.com",
-#         msg=f"Subject:Monday Motivation\n\nhellooooo"
-#     )
-
-
-# connection = smtplib.SMTP("smtp.gmail.com")
-# connection.starttls()
-# connection.login(user=my_email, password=password)
-#
-# connection.sendmail(from_addr=my_email, to_addrs="kaaf_kaaf@hotmail.com", msg="Hello")
-#
-# connection.close()
-
+        print('Email sent!')
+    except:
+        print('Something went wrong...')
